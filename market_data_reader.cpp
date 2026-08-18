d5c496852c40b186d10ffd119029b82e3898aa03
cmoon4799 <cmoon4799@gmail.com>
2026-08-17 22:45:52 -0700

    add: trade data parsing

--- market_data_reader.cpp
+++ market_data_reader.cpp
@@ -11,31 +11,47 @@
  * Explicitly map string to a Symbol enum. Although the symbols in the dataset are of the form SYNTH
  * + N, we want to avoid coupling text format to enum ordinal values.
  */
-Symbol parse_symbol(std::string_view symbol_text)
+Symbol parse_symbol(std::string_view symbol_string)
 {
-    if (symbol_text == "SYNTH1")
+    if (symbol_string == "SYNTH1")
     {
         return Symbol::SYNTH1;
     }
-    else if (symbol_text == "SYNTH2")
+    else if (symbol_string == "SYNTH2")
     {
         return Symbol::SYNTH2;
     }
-    else if (symbol_text == "SYNTH3")
+    else if (symbol_string == "SYNTH3")
     {
         return Symbol::SYNTH3;
     }
-    else if (symbol_text == "SYNTH4")
+    else if (symbol_string == "SYNTH4")
     {
         return Symbol::SYNTH4;
     }
-    else if (symbol_text == "SYNTH5")
+    else if (symbol_string == "SYNTH5")
     {
         return Symbol::SYNTH5;
     }
     else
     {
-        throw std::invalid_argument(std::format("Cannot parse symbol: {}", symbol_text));
+        throw std::invalid_argument(std::format("Cannot parse symbol: {}", symbol_string));
+    }
+}
+
+Aggressor parse_aggressor(std::string_view aggressor_string)
+{
+    if (aggressor_string == "B")
+    {
+        return Aggressor::BUY;
+    }
+    else if (aggressor_string == "S")
+    {
+        return Aggressor::SELL;
+    }
+    else
+    {
+        throw std::invalid_argument(std::format("Cannot parse aggressor: {}", aggressor_string));
     }
 }
 
@@ -100,4 +116,67 @@ std::vector<QuoteData> parse_quote_data_from_csv(std::string_view csv_path)
     }
 
     return quotes;
+}
+
+std::vector<TradeData> parse_trade_data_from_csv(std::string_view csv_path)
+{
+    std::ifstream file(std::string{csv_path});
+    if (!file)
+    {
+        throw std::runtime_error("Failed to read market data csv");
+    }
+
+    std::vector<TradeData> trades;
+
+    std::string line;
+    while (std::getline(file, line))
+    {
+        // Ignore header comments and the CSV column header row
+        if (line.size() > 0 && (line[0] == '#' || line.starts_with("Timestamp,")))
+        {
+            continue;
+        }
+
+        std::istringstream line_stream{line};
+        std::string field;
+
+        // Extract timestamp
+        std::chrono::nanoseconds timestamp;
+        line_stream >> std::chrono::parse("%T", timestamp); // %T expects form HH:MM:SS:sss
+
+        // Extract market data type
+        std::getline(line_stream, field, ','); // Consume leftover comma after parsing timestamp
+        std::getline(line_stream, field, ',');
+        if (field != "T")
+        {
+            continue;
+        }
+
+        // Extract symbol
+        std::getline(line_stream, field, ',');
+        Symbol symbol{parse_symbol(field)};
+
+        // Ignore bid price, bid quantity, ask price, ask quantity
+        std::getline(line_stream, field, ',');
+        std::getline(line_stream, field, ',');
+        std::getline(line_stream, field, ',');
+        std::getline(line_stream, field, ',');
+
+        // Extract price
+        std::getline(line_stream, field, ',');
+        Price price{field};
+
+        // Extract quantity
+        std::getline(line_stream, field, ',');
+        Quantity quantity;
+        std::from_chars(field.data(), field.data() + field.size(), quantity);
+
+        // Extract aggressor
+        std::getline(line_stream, field, ',');
+        Aggressor aggressor{parse_aggressor(field)};
+
+        trades.push_back(TradeData{timestamp, symbol, price, quantity, aggressor});
+    }
+
+    return trades;
 }
--- market_data_reader.hpp
+++ market_data_reader.hpp
@@ -18,15 +18,14 @@ struct TradeData
 {
     std::chrono::nanoseconds timestamp;
     Symbol symbol;
-    Quantity quantity;
     Price price;
+    Quantity quantity;
     Aggressor aggressor;
 };
 
 /*
  * The CSV should be newline separated market quotes and trade prints. Market quotes should be of
- * the form: HH:MM:SS.sss,Q,SYMBOL,BID_PRICE,BID_QTY,ASK_PRICE,ASK_QTY. Trade prints should be of
- * the form:
+ * the form: HH:MM:SS.sss,Q,SYMBOL,BID_PRICE,BID_QTY,ASK_PRICE,ASK_QTY.
  *
  * An assumption we make about the data is that the time range of the data does not span longer
  * than 24 hours. Otherwise, because timestamps are of the form HH:MM:SS, timestamps that occur at
@@ -34,4 +33,12 @@ struct TradeData
  */
 std::vector<QuoteData> parse_quote_data_from_csv(std::string_view csv_path);
 
+/*
+ * The CSV should be newline separated market quotes and trade prints. Trade prints should be of
+ * the form: HH:MM:SS.sss,T,SYMBOL,,,,,PRICE,QTY,AGGRESSOR.
+ *
+ * An assumption we make about the data is that the time range of the data does not span longer
+ * than 24 hours. Otherwise, because timestamps are of the form HH:MM:SS, timestamps that occur at
+ * the same time on different days are ambiguous.
+ */
 std::vector<TradeData> parse_trade_data_from_csv(std::string_view csv_path);
